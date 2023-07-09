5d66f0e09095f95201211a184cf00e1591e08cac
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-07-09 15:08:43 -0400

    3

--- controllers/cards.js
+++ controllers/cards.js
@@ -60,7 +60,6 @@ const deleteCard = (req, res) => {
     });
 };
 const likeCard = (req, res) => {
-  const { cardId } = req.params;
   const { _id } = req.user;
   Card.findByIdAndUpdate(
     req.params.cardId,
@@ -68,10 +67,10 @@ const likeCard = (req, res) => {
     { new: true, runValidators: true },
   )
     .then((card) => {
-      if (!cardId) {
+      if (req.params.cardId === 'CastError') {
         res
           .status(ERROR_NOT_FOUND)
-          .send({ massage: 'Запрашиваемая карточка не найдена' });
+          .send({ massage: 'Запрашиваемая карточка не найдена' })
       }
       res
         .status(STATUS_OK)
