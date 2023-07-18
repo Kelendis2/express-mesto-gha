491da705a17224361e75f48b08c93bc05ddcf891
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-07-18 10:52:00 -0400

    1

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,10 +1,4 @@
 const errorHandler = (err, req, res, next) => {
-  if (err instanceof celebrate.ValidationError) {
-    // Обработка ошибок валидации
-    return res.status(400).json({
-      message: 'Переданы некорректные данные при создании пользователя.',
-    });
-  }
   // если нет статуса выставляем 500
   const { statusCode = 500, message } = err;
 
