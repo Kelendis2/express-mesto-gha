007e53cba61e0cfdfee8bf9d54a2d9e0d175e2a6
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-07-17 23:12:29 -0400

    14/13

--- utils/validate.js
+++ utils/validate.js
@@ -1,28 +1,28 @@
-const { celebrate, Joi, Segments } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 
 const regex = /^(https?:\/\/)?[^\s]*\.(jpg|jpeg|png|gif|bmp|test)$/;
 
 module.exports.validateUser = celebrate({
-  [Segments.BODY]: Joi.object().keys({
+  params: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     about: Joi.string().min(2).max(30).required(),
   }),
 });
 
 module.exports.validateUserAvatar = celebrate({
-  [Segments.BODY]: Joi.object().keys({
+  body: Joi.object().keys({
     avatar: Joi.string().pattern(regex).required(),
   }),
 });
 
 module.exports.validateUserID = celebrate({
-  [Segments.PARAMS]: Joi.object().keys({
+  params: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
   }),
 });
 
 module.exports.validateCardID = celebrate({
-  [Segments.PARAMS]: Joi.object().keys({
+  params: Joi.object().keys({
     cardId: Joi.string().length(24).hex().required(),
   }),
 });
@@ -35,7 +35,7 @@ module.exports.validateCard = celebrate({
 });
 
 module.exports.validateUserCreate = celebrate({
-  [Segments.BODY]: Joi.object().keys({
+  body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
@@ -45,7 +45,7 @@ module.exports.validateUserCreate = celebrate({
 });
 
 module.exports.validateUserAuth = celebrate({
-  [Segments.BODY]: Joi.object().keys({
+  body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
