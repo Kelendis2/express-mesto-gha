6694c4132cc1f23f0b0773068692a3ae2fc8d834
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-07-17 21:11:31 -0400

    14/9

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,18 +2,18 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/constants');
 
 const auth = (req, res, next) => {
-  const { token } = req.cookies.jwt;
-  let payload;
-
-  try {
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (err) {
-    next(err);
+  const { jwt: { token } = {} } = req.cookies;
+  if (!token) {
+    return res.status(401).json({ message: 'Требуется аутентификация' });
   }
 
-  req.user = payload;
-
-  next();
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Неверный токен аутентификации' });
+  }
 };
 
 module.exports = auth;
