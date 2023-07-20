f1098a82c24b767c32251527c127a7c0a9c82f3a
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-07-20 11:04:19 -0400

    2

--- middlewares/error.js
+++ middlewares/error.js
@@ -6,6 +6,7 @@ const errorHandler = (err, req, res, next) => {
     // проверяем статус и выставляем сообщение в зависимости от него
     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
+  next();
 };
 
 module.exports = errorHandler;
