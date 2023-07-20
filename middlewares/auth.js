42629efa9d47d94db0133deaccc006e5f031165b
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-07-20 14:50:23 -0400

    1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/constants');
+const ErrorAccess = require('../utils/errors/ErrorAccess');
 
+const handleAuthError = (req, res, next) => next(new ErrorAccess('Необходима авторизация'));
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
   const { token } = req.cookies;
   try {
     if (!token) {
-      return res.status(401).json({ message: 'Требуется аутенфикация' });
+      return handleAuthError(req, res, next);
     }
     const payload = jwt.verify(token, JWT_SECRET);
     req.user = payload;
     next();
   } catch (err) {
-    return res.status(401).json({ message: 'Неверный токен аутентификации' });
+    return handleAuthError(req, res, next);
   }
 };
 
